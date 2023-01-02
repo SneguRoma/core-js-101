5e3d479d15859db0f917d88b965e524090e08841
sneguroma <sneguroma@gmail.com>
2023-01-03 00:09:36 +0100

    feat: 78 passing

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -169,8 +169,9 @@ function partialUsingArguments(/* fn, ...args1 */) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let i = startFrom - 1;
+  return () => { i += 1; return i; };
 }
 
 
